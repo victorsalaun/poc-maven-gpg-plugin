f147281f16d5b90be787bfb234a39bb08cc777a8
victorsalaun <victor.salaun@gmail.com>
2018-05-14 21:42:47 +0200

    init maven project (with spring boot)

---
+++ src/main/java/com/example/pocmavengpgplugin/PocMavenGpgPluginApplication.java
@@ -0,0 +1,12 @@
+package com.example.pocmavengpgplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMavenGpgPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocMavenGpgPluginApplication.class, args);
+    }
+}
